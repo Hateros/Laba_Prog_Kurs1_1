28c0192854488562d0e60ba0b13a840a32f0b7c9
Hateros <40269649+Hateros@users.noreply.github.com>
2021-10-18 13:19:17 +0300

    Add files via upload

---
+++ Laba_1_8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Laba_1_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x:");
+            double x = double.Parse(Console.ReadLine());
+            double rez = (((3 * x - 5) * x + 2) * x - 1) * x + 7;
+            Console.WriteLine("Результат вычислений = " + rez);
+        }
+    }
+}
