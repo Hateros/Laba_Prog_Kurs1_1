bafa55ec82929686f7b7a57bc7a16ff7ee36214c
Hateros <40269649+Hateros@users.noreply.github.com>
2021-10-18 13:17:48 +0300

    Add files via upload

---
+++ Laba_1_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Laba_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, s, p;
+            Console.WriteLine("Введите длину первого катета:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину второго катета");
+            b = double.Parse(Console.ReadLine());
+            c = Math.Sqrt(a * a + b * b);
+            s = 0.5 * a * b;
+            p = a + b + c;
+            Console.WriteLine("Периметр = {0}, Площадь = {1}", p, s);
+        }
+    }
+}
