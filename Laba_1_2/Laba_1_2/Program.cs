376226e5308f83adba544487c5ea1fbeed0fbcf8
Hateros <40269649+Hateros@users.noreply.github.com>
2021-10-18 13:14:03 +0300

    Add files via upload

---
+++ Laba_1_2/Laba_1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Laba_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько прошло секунд?");
+            int sec = int.Parse(Console.ReadLine());
+            int hour = sec / 3600;
+            int min = (sec - hour * 3600) / 60;
+            Console.WriteLine("Прошло {0} часов и {1} минут.", hour, min);
+        }
+    }
+}
