99586f60f60a40c142829aacc4fcb8bbe7da5279
Hateros <40269649+Hateros@users.noreply.github.com>
2021-10-18 13:12:54 +0300

    Add files via upload

---
+++ Laba_1_1/Laba_1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Laba_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите вещественное число:");
+            string chisl = Console.ReadLine();
+            double x = double.Parse(chisl);
+            int d = (int)(x * 10 % 10);
+            Console.WriteLine("Первая цифра из дробной части: " + d);
+        }
+    }
+}
