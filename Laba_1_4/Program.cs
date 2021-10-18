d99d967af4dc37a7847adf7d7095154523255f05
Hateros <40269649+Hateros@users.noreply.github.com>
2021-10-18 13:17:11 +0300

    Add files via upload

---
+++ Laba_1_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laba_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую переменную:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вторую переменную:");
+            int b = int.Parse(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("Новое значение первой переменной = {0}; второй = {1}", a, b);
+        }
+    }
+}
