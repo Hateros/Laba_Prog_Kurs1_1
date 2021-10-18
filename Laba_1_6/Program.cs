0b2ae2bd094304c1ac6ff0bd4d94fe5be0f308d6
Hateros <40269649+Hateros@users.noreply.github.com>
2021-10-18 13:18:19 +0300

    Add files via upload

---
+++ Laba_1_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Laba_1_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, first, second, third, fourth, proisv;
+            Console.WriteLine("Введите четырехзначное число:");
+            number = int.Parse(Console.ReadLine());
+            first = number / 1000;
+            second = (number - first * 1000) / 100;
+            third = (number - first * 1000 - second * 100) / 10;
+            fourth = (number - first * 1000 - second * 100 - third * 10);
+            proisv = first * second * third * fourth;
+            Console.WriteLine("Произведение чисел = " + proisv);
+
+        }
+    }
+}
