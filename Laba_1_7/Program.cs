67ce77cd9ac25ac0839bf9d1585abf47b5cfc77c
Hateros <40269649+Hateros@users.noreply.github.com>
2021-10-18 13:18:46 +0300

    Add files via upload

---
+++ Laba_1_7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Laba_1_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo, fi, se, th;
+            Console.WriteLine("Введите трехзначное число:");
+            chislo = int.Parse(Console.ReadLine());
+            fi = chislo / 100;
+            se = (chislo - fi * 100) / 10;
+            th = (chislo - fi * 100 - se * 10);
+            Console.WriteLine("Число наоборот: " + th + se + fi);
+        }
+    }
+}
