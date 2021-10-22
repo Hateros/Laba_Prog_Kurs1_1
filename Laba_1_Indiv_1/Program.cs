f9bf3c2139f1194c95c14e122be394943ada8578
Hateros <40269649+Hateros@users.noreply.github.com>
2021-10-22 09:42:02 +0300

    Update Program.cs

--- Laba_1_Indiv_1/Program.cs
+++ Laba_1_Indiv_1/Program.cs
@@ -31,12 +31,12 @@ namespace Laba_1_Indiv_1_v2
             Console.WriteLine("Строй государства (Ф - федерация, УГ - унитарное государство):");
             string ex34 = Console.ReadLine();
             Console.WriteLine();
-            Console.WriteLine("География");
-            Console.WriteLine("Государство\tСтолица\t\tНаселение\t\tСтрой");
-            Console.WriteLine("{0}\t{1}\t{2}\t\t{3}", ex11, ex12, ex13, ex14);
-            Console.WriteLine("{0}\t\t{1}\t\t{2}\t\t\t{3}", ex21, ex22, ex23, ex24);
-            Console.WriteLine("{0}\t\t{1}\t\t{2}\t\t{3}", ex31, ex32, ex33, ex34);
-            Console.WriteLine("Перечисляемый тип: Ф - федерация, УГ - унитарное государство");
+            Console.WriteLine("\tГеография");
+            Console.WriteLine("\tГосударство\t\tСтолица\t\tНаселение\t\tСтрой");
+            Console.WriteLine("\t{0}\t\t{1}\t\t{2}\t\t{3}", ex11, ex12, ex13, ex14);
+            Console.WriteLine("\t{0}\t\t{1}\t\t{2}\t\t{3}", ex21, ex22, ex23, ex24);
+            Console.WriteLine("\t{0}\t\t{1}\t\t{2}\t\t{3}", ex31, ex32, ex33, ex34);
+            Console.WriteLine("\tПеречисляемый тип: Ф - федерация, УГ - унитарное государство");
             Console.ReadKey();
 
         }
